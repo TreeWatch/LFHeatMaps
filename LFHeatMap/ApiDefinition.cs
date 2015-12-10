e74019306d6e7d6d09a5567486d825862bb1898b
Jan Kerkenhoff <Jan.Kerkenhoff@gmail.com>
2015-12-10 10:00:41 +0100

    Fixed lib not automatically compiling

--- LFHeatMap/ApiDefinition.cs
+++ LFHeatMap/ApiDefinition.cs
@@ -12,16 +12,16 @@ namespace LFHeatMap
 		// +(UIImage *)heatMapForMapView:(MKMapView *)mapView boost:(float)boost locations:(NSArray *)locations weights:(NSArray *)weights;
 		[Static]
 		[Export ("heatMapForMapView:boost:locations:weights:")]
-		UIImage HeatMapForMapView (MKMapView mapView, float boost, CGPoint[] locations, NSNumber[] weights);
+        UIImage HeatMapForMapView (MKMapView mapView, float boost, NSObject[] locations, NSNumber[] weights);
 
 		// +(UIImage *)heatMapWithRect:(CGRect)rect boost:(float)boost points:(NSArray *)points weights:(NSArray *)weights;
 		[Static]
 		[Export ("heatMapWithRect:boost:points:weights:")]
-		UIImage HeatMapWithRect (CGRect rect, float boost, CGPoint[] points, NSNumber[] weights);
+        UIImage HeatMapWithRect (CGRect rect, float boost, NSObject[] points, NSNumber[] weights);
 
 		// +(UIImage *)heatMapWithRect:(CGRect)rect boost:(float)boost points:(NSArray *)points weights:(NSArray *)weights weightsAdjustmentEnabled:(BOOL)weightsAdjustmentEnabled groupingEnabled:(BOOL)groupingEnabled;
 		[Static]
 		[Export ("heatMapWithRect:boost:points:weights:weightsAdjustmentEnabled:groupingEnabled:")]
-		UIImage HeatMapWithRect (CGRect rect, float boost, CGPoint[] points, NSNumber[] weights, bool weightsAdjustmentEnabled, bool groupingEnabled);
+        UIImage HeatMapWithRect (CGRect rect, float boost, NSObject[] points, NSNumber[] weights, bool weightsAdjustmentEnabled, bool groupingEnabled);
 	}
 }
