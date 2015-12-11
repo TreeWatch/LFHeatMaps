eadd7feb27d1b1c1043047090d1ae90876c76761
Jan Kerkenhoff <Jan.Kerkenhoff@gmail.com>
2015-12-11 11:02:02 +0100

    Updated commands

--- LFHeatMap/ApiDefinition.cs
+++ LFHeatMap/ApiDefinition.cs
@@ -17,11 +17,11 @@ namespace LFHeatMap
 		// +(UIImage *)heatMapWithRect:(CGRect)rect boost:(float)boost points:(NSArray *)points weights:(NSArray *)weights;
 		[Static]
 		[Export ("heatMapWithRect:boost:points:weights:")]
-        UIImage HeatMapWithRect (CGRect rect, float boost, NSObject[] points, NSNumber[] weights);
+        UIImage HeatMapWithRect (CGRect rect, float boost, NSValue[] points, NSNumber[] weights);
 
 		// +(UIImage *)heatMapWithRect:(CGRect)rect boost:(float)boost points:(NSArray *)points weights:(NSArray *)weights weightsAdjustmentEnabled:(BOOL)weightsAdjustmentEnabled groupingEnabled:(BOOL)groupingEnabled;
 		[Static]
 		[Export ("heatMapWithRect:boost:points:weights:weightsAdjustmentEnabled:groupingEnabled:")]
-        UIImage HeatMapWithRect (CGRect rect, float boost, NSObject[] points, NSNumber[] weights, bool weightsAdjustmentEnabled, bool groupingEnabled);
+        UIImage HeatMapWithRect (CGRect rect, float boost, NSValue[] points, NSNumber[] weights, bool weightsAdjustmentEnabled, bool groupingEnabled);
 	}
 }
